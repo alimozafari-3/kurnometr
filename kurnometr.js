564e8df7bc4a661dcc11f0337e3b35562c6b76b2
ali <ali.mozafari733@yahoo.com>
2023-04-17 18:13:04 +0430

    add js Dom

--- kurnometr.js
+++ kurnometr.js
@@ -1,1 +1,10 @@
 
+let h = document.querySelector(".hour");
+let m = document.querySelector(".mantue");
+let s = document.querySelector(".second");
+
+let hh = 0;
+let mm = 0;
+let ss = 0;
+
+let interval;
