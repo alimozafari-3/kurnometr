a43123716cea77c24f3fbb6e92af1cf2bbb6a5ac
ali <ali.mozafari733@yahoo.com>
2023-04-19 01:01:55 +0430

    add setinterval

--- kurnometr.js
+++ kurnometr.js
@@ -1,4 +1,3 @@
-
 let h = document.querySelector(".hour");
 let m = document.querySelector(".mantue");
 let s = document.querySelector(".second");
@@ -8,3 +7,21 @@ let mm = 0;
 let ss = 0;
 
 let interval;
+
+function start() {
+    interval = setInterval(() => {
+      if (ss < 59) {
+        ss += 1;
+        less10(ss, s);
+      } else {
+        ss = 0;
+        if (mm < 59) {
+          mm += 1;
+          less10(mm, m);
+        } else {
+          hh += 1;
+          less10(hh, h);
+        }
+      }
+    }, 1000);
+  }
